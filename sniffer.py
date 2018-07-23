e6b07d785b992db514aab805a6f8149c1f1129da
Jesse Williams <jessenw@Jesses-MacBook-Pro.local>
2018-07-23 21:16:26 +1200

    Added assignment example code

---
+++ sniffer.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+'''
+sniffer.py -- Python packet sniffer
+
+By Jordan Ansell (jordan.ansell@ecs.vuw.ac.nz) July 2017
+
+Use as-is, modification, and/or inclusion in derivative works is permitted only if the original author is credited.
+
+Requires the pcapy library.
+To install: python3 -m pip install pcapy -t ./pcapy/
+
+To run:     python3 sniffer.py packets_file.pcap
+Or:         tcpdump -w - | python3 ./sniffer.py -
+'''
+
+from pcapy import pcapy  
+
+import binascii
+import struct
+import sys
+import ipaddress
+
+def mac2str(mac_bytes):
+    mac_string = binascii.hexlify(mac_bytes).decode('ascii')
+    mac_pairs = [i+j for i,j in zip(mac_string[0::2], mac_string[1::2])]
+    return ':'.join(mac_pairs)
+
+class PacketHeaderBase:
+    ''' Base class for packet headers. '''
+
+    def __init__(self, hdr_format, field_names, data):
+        ''' Creates a header object from the packet data given.
+            Field names are given as an array of strings.
+            For more information on struct format strings see:
+              https://docs.python.org/3/library/struct.html '''
+
+        self.data = data
+        self.hdr_length = struct.calcsize(hdr_format)
+        self.field_values = struct.unpack(
+            hdr_format, 
+            data[:self.hdr_length])
+
+        pkt_dict = dict(zip(field_names, self.field_values))
+        for k, v in pkt_dict.items():
+            setattr(self, k, v)
+
+class Ethernet(PacketHeaderBase):
+    ''' Ethernet header class. '''
+
+    # _fmt and _fields define the structure of an Ethernet packet
+    fmt = ''  # TODO: format string for Ethernet
+    fields = [  ]  # TODO: list of Ethernet fields
+
+    def __init__(self, data):
+        super().__init__(Ethernet.fmt, Ethernet.fields, data)
+
+    def __str__(self):
+        return "Ethernet payload {}".format( binascii.hexlify(self.payload) )  # what information needs to be printed?
+
+def process_packet(packet_data):
+    ''' Function for processing a single packet '''
+    parsed_pkt = dict()
+
+    # print( binascii.hexlify(packet_data) )
+
+    # process the datalink header
+    parsed_pkt['ethernet'] = Ethernet(packet_data)
+
+    # use Ethernet header to decide what the next header type is, process that header.
+
+def main(pcap_filename):
+    ''' Main function, loops over packets in the given file, counting and processing each. '''
+    print( "Opening file: '{}'".format(pcap_filename) )
+    pcap_reader = pcapy.open_offline( pcap_filename )
+
+    count = 0
+    while True:
+        meta, data = pcap_reader.next()
+        if len(data) < 1:
+            break  # no more packets 
+        
+        count += 1
+        print("#### Packet {}:".format(count))
+        process_packet( data )
+
+    print("Complete. {} packets processed.".format(count))
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Please supply pcap file argument: python3 sniffer.py packets.pcap")
+        exit()
+
+    main(sys.argv[1])
