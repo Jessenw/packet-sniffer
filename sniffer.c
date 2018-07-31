f2f0256a131041c46a4a1e8ed88142c6c9abc3b2
Jesse Williams <williajess7@ecs.vuw.ac.nz>
2018-07-31 20:10:18 +1200

    implemented ip and ipv4 handling. Also began on mac2str conversion

--- sniffer.c
+++ sniffer.c
@@ -14,10 +14,35 @@
 
 #include <stdio.h>
 #include <pcap.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/if_ether.h>
 
+const struct ether_header *ether;
+
+char* mac2str(const char* arr)
+{
+    char mac_str[18]; // stored mac address
+
+    if(arr == NULL) return "";
+    snprintf(mac_str, sizeof(mac_str), "%02x:%02x:%02x:%02x:%02x:%02x",
+         arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]);
+
+    return mac_str;
+}
+
+// https://unix.superglobalmegacorp.com/Net2/newsrc/netinet/if_ether.h.html
 void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 {
-    printf("Header Length: %d\n", header->len);
+    char* src_mac_addr = mac2str(ether->ether_shost);
+    printf("%s\n", src_mac_addr);
+    //printf("Source MAC: %s | Destination MAC: %s\n", ether->ether_shost, ether->ether_dhost);
+    if(ether->ether_type == ntohs(ETHERTYPE_IP)) {
+        printf("Ethernet Protocol: IPv4\n");
+    } else if(ether->ether_type == ntohs(ETHERTYPE_IPV6)) {
+        printf("Ethernet Protocol: IPv6\n");
+    }
+    printf("\n");
 }
 
 int main(int argc, char **argv)
@@ -28,9 +53,8 @@ int main(int argc, char **argv)
     }
 
     pcap_t *handle = pcap_open_offline(argv[1], NULL);
-    // pcap_loop(pcap_t *p, int cnt, pcap_handler callback, u_char *user);
     pcap_loop(handle, 1024*1024, got_packet, NULL);
-    pcap_close(handle); 
+    pcap_close(handle);
 
     return 0;
 }
