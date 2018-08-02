1c98eaea474a4f764f8ac9669db00d918f9b8f86
Jesse Williams <williajess7@ecs.vuw.ac.nz>
2018-08-02 14:26:09 +1200

    added tcp handler from sniffex.c

--- sniffer.c
+++ sniffer.c
@@ -36,11 +36,30 @@
 #define ETHER_ADDR_LEN 6
 
 /* Ethernet header */
-	struct sniff_ethernet {
-		u_char ether_dhost[ETHER_ADDR_LEN]; /* Destination host address */
-		u_char ether_shost[ETHER_ADDR_LEN]; /* Source host address */
-		u_short ether_type; /* IP? ARP? RARP? etc */
-	};
+struct sniff_ethernet {
+	u_char ether_dhost[ETHER_ADDR_LEN]; /* Destination host address */
+	u_char ether_shost[ETHER_ADDR_LEN]; /* Source host address */
+	u_short ether_type;                 /* IPv4, IPv6, unknown */
+};
+
+/* IP header */
+struct sniff_ip {
+        u_char  ip_vhl;                 /* version << 4 | header length >> 2 */
+        u_char  ip_tos;                 /* type of service */
+        u_short ip_len;                 /* total length */
+        u_short ip_id;                  /* identification */
+        u_short ip_off;                 /* fragment offset field */
+        #define IP_RF 0x8000            /* reserved fragment flag */
+        #define IP_DF 0x4000            /* dont fragment flag */
+        #define IP_MF 0x2000            /* more fragments flag */
+        #define IP_OFFMASK 0x1fff       /* mask for fragmenting bits */
+        u_char  ip_ttl;                 /* time to live */
+        u_char  ip_p;                   /* protocol */
+        u_short ip_sum;                 /* checksum */
+        struct  in_addr ip_src,ip_dst;  /* source and dest address */
+};
+
+#define IP_HL(ip)(((ip)->ip_vhl) & 0x0f)
 
 /*
  * args = user arguments, in this case NULL from pcap_loop.
@@ -50,10 +69,13 @@
  */
 void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 {
+    printf("\n");
     static int count = 1;
 
     const struct sniff_ethernet *ethernet; /* Ethernet handler function */
     const struct sniff_ip *ip;
+
+    int size_ip;
 
     printf("Packet #%d\n", count);
     count++;
@@ -74,6 +96,34 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
     }
 
     ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
+    size_ip = IP_HL(ip)*4;
+    if (size_ip < 20) {
+		printf("   * Invalid IP header length: %u bytes\n", size_ip);
+		return;
+	}
+
+    /* print source and destination IP addresses */
+	printf("From: %s\n", inet_ntoa(ip->ip_src));
+	printf("To: %s\n", inet_ntoa(ip->ip_dst));
+	
+	/* determine protocol */	
+	switch(ip->ip_p) {
+		case IPPROTO_TCP:
+			printf("Protocol: TCP\n");
+			break;
+		case IPPROTO_UDP:
+			printf("Protocol: UDP\n");
+			return;
+		case IPPROTO_ICMP:
+			printf("Protocol: ICMP\n");
+			return;
+		case IPPROTO_IP:
+			printf("Protocol: IP\n");
+			return;
+		default:
+			printf("Protocol: unknown\n");
+			return;
+	}
 }
 
 int main(int argc, char **argv)
