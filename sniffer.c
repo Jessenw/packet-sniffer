b6aab78cee9c9e6ad3ea464e41c0b2b7dd17bd57
Jesse Williams <williajess7@ecs.vuw.ac.nz>
2018-08-02 14:41:53 +1200

    added print payload from sniffex.c

--- sniffer.c
+++ sniffer.c
@@ -59,41 +59,82 @@ struct sniff_ip {
         struct  in_addr ip_src,ip_dst;  /* source and dest address */
 };
 
+/* TCP header */
+typedef u_int tcp_seq;
+
+struct sniff_tcp {
+        u_short th_sport;               /* source port */
+        u_short th_dport;               /* destination port */
+        tcp_seq th_seq;                 /* sequence number */
+        tcp_seq th_ack;                 /* acknowledgement number */
+        u_char  th_offx2;               /* data offset, rsvd */
+#define TH_OFF(th)      (((th)->th_offx2 & 0xf0) >> 4)
+        u_char  th_flags;
+        #define TH_FIN  0x01
+        #define TH_SYN  0x02
+        #define TH_RST  0x04
+        #define TH_PUSH 0x08
+        #define TH_ACK  0x10
+        #define TH_URG  0x20
+        #define TH_ECE  0x40
+        #define TH_CWR  0x80
+        #define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+        u_short th_win;                 /* window */
+        u_short th_sum;                 /* checksum */
+        u_short th_urp;                 /* urgent pointer */
+};
+
 #define IP_HL(ip)(((ip)->ip_vhl) & 0x0f)
 
 /*
- * args = user arguments, in this case NULL from pcap_loop.
- * header = information about when the packet was sniffed.
- * packet = pointer to the first byte of a chunk of data containing the entire packet
- *          packet should be thought as a collection of structs instead of a string.
+ * print packet payload data (avoid printing binary data)
  */
-void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
+void
+print_payload(const u_char *payload, int len)
 {
-    printf("\n");
-    static int count = 1;
 
-    const struct sniff_ethernet *ethernet; /* Ethernet handler function */
+	int len_rem = len;
+	int line_width = 16;			/* number of bytes per line */
+	int line_len;
+	int offset = 0;					/* zero-based offset counter */
+	const u_char *ch = payload;
+
+	if (len <= 0)
+		return;
+
+	/* data fits on one line */
+	if (len <= line_width) {
+		print_hex_ascii_line(ch, len, offset);
+		return;
+	}
+
+	/* data spans multiple lines */
+	for ( ;; ) {
+		/* compute current line length */
+		line_len = line_width % len_rem;
+		/* print line */
+		print_hex_ascii_line(ch, line_len, offset);
+		/* compute total remaining */
+		len_rem = len_rem - line_len;
+		/* shift pointer to remaining bytes to print */
+		ch = ch + line_len;
+		/* add offset */
+		offset = offset + line_width;
+		/* check if we have line width chars or less */
+		if (len_rem <= line_width) {
+			/* print last line and get out */
+			print_hex_ascii_line(ch, len_rem, offset);
+			break;
+		}
+	}
+
+    return;
+}
+
+void ipv4_handler(const u_char *packet)
+{
     const struct sniff_ip *ip;
-
     int size_ip;
-
-    printf("Packet #%d\n", count);
-    count++;
-
-    ethernet = (struct sniff_ethernet*)(packet); /* Type case packet to ethernet header */
-
-    /* IPv4 */
-    if(ntohs(ethernet->ether_type) == 2048) {
-        printf("Protocol: IPv4\n");
-    }
-    /* IPv6 */
-    else if(ntohs(ethernet->ether_type) == 34525) {
-        printf("Protocol: IPv6\n");
-    } 
-    /* Unknown */
-    else {
-        printf("Protocol: Unknown\n");
-    }
 
     ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
     size_ip = IP_HL(ip)*4;
@@ -124,6 +165,73 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
 			printf("Protocol: unknown\n");
 			return;
 	}
+}
+
+void tcp_handler(const u_char *packet, const int size_ip_)
+{
+    const struct sniff_tcp *tcp; /* The TCP header */
+
+    int size_ip = size_ip_;
+    int size_tcp;
+    int size_payload;
+
+    tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
+	size_tcp = TH_OFF(tcp)*4;
+	if (size_tcp < 20) {
+		printf("* Invalid TCP header length: %u bytes\n", size_tcp);
+		return;
+	}
+	
+	printf("Src port: %d\n", ntohs(tcp->th_sport));
+	printf("Dst port: %d\n", ntohs(tcp->th_dport));
+	
+	/* define/compute tcp payload (segment) offset */
+	payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);
+	
+	/* compute tcp payload (segment) size */
+	size_payload = ntohs(ip->ip_len) - (size_ip + size_tcp);
+	
+	/*
+	 * Print payload data; it might be binary, so don't just
+	 * treat it as a string.
+	 */
+	if (size_payload > 0) {
+		printf("Payload (%d bytes):\n", size_payload);
+		print_payload(payload, size_payload);
+	}
+}
+
+/*
+ * args = user arguments, in this case NULL from pcap_loop.
+ * header = information about when the packet was sniffed.
+ * packet = pointer to the first byte of a chunk of data containing the entire packet
+ *          packet should be thought as a collection of structs instead of a string.
+ */
+void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
+{
+    printf("\n");
+    static int count = 1;
+
+    const struct sniff_ethernet *ethernet; /* Ethernet handler function */
+
+    printf("Packet #%d\n", count);
+    count++;
+
+    ethernet = (struct sniff_ethernet*)(packet); /* Type case packet to ethernet header */
+
+    /* IPv4 */
+    if(ntohs(ethernet->ether_type) == 2048) {
+        printf("Protocol: IPv4\n");
+        ipv4_handler(packet);
+    }
+    /* IPv6 */
+    else if(ntohs(ethernet->ether_type) == 34525) {
+        printf("Protocol: IPv6\n");
+    } 
+    /* Unknown */
+    else {
+        printf("Protocol: Unknown\n");
+    }
 }
 
 int main(int argc, char **argv)
